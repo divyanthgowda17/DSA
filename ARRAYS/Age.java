6868e6d71cc18632635f75aacc6f22da734cf78d
divyanthgowda17 <gowdadivyanth@gmail.com>
2026-07-11 23:21:15 +0530

    Added vote eligibity to strengthen basics

---
+++ ARRAYS/Age.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Age{
+public static void main (String args[]){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter age:");
+    int age = sc.nextInt();
+
+    if(age>=18){
+        System.out.println("Adult: Eligible to vote");
+    }
+    else{
+        System.out.println("NOT ADULT");
+    }
+    sc.close();
+}
+}
